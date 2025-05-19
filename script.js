7e86a150945387d9fe49d548f8da7853cebec372
Momzalp <166235303+Momzalp@users.noreply.github.com>
2025-05-19 13:38:15 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,5 +13,24 @@ function mostrarPago() {
   if (metodo === "pagoefectivo") {
     alert("❌ PagoEfectivo no está disponible actualmente.");
     return;
-  
+  }
+
+  let precio;
+  switch(diamonds) {
+    case "341": precio = "S/7.01"; break;
+    case "572": precio = "S/11.68"; break;
+    case "1166": precio = "S/23.29"; break;
+    case "2398": precio = "S/45.95"; break;
+    case "6160": precio = "S/116.82"; break;
+    default: precio = "S/0.00";
+  }
+
+  pedido = {
+    playerId,
+    diamonds,
+    precio
+  };
+
+  document.getElementById("main-section").style.display = "none";
+  document.getElementById("payment-section").style.display = "block";
 }
